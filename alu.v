ff2851c4963190f334d21d3cfd280b269ad81e5b
SangwookBok <joeer7410@gmail.com>
2021-09-13 13:13:22 -0400

    fea: alu w/o mux

--- alu.v
+++ alu.v
@@ -5,7 +5,21 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 
    output [31:0] data_result;
    output isNotEqual, isLessThan, overflow;
+	
+	wire [31:0] Bx, Bmux;
+	wire cout; //We need cout?
+	
+	genvar i;
 
-   // YOUR CODE HERE //
+   //xor xor1(Bx, data_operandB, {32{ctrl_ALUopcode[0]});
+	//assign Bx = ~data_operandB;
+	generate
+		for (i=0;i<32;i=i+1) begin: notgate
+			not(Bx[i], data_operandB[i]);
+		end
+	endgenerate
+	
+	mux_2to1_32bit mux1(data_operandB, Bx, ctrl_ALUopcode[0], Bmux);
+	CSA_32bit csa(data_operandA, Bmux, ctrl_ALUopcode[0], data_result, cout, overflow);
 
 endmodule
